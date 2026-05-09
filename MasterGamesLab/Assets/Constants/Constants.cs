267b9d4e9968032a5e5d9bc9a413f902912a52b7
mulsicpp <christianmulser000@gmail.com>
2026-05-09 18:44:50 +0200

    added file containing constants

---
+++ MasterGamesLab/Assets/Constants/Constants.cs
@@ -0,0 +1,9 @@
+using System.Drawing;
+
+public class Constants
+{
+    public const int MIN_PLAYER_COUNT = 2;
+    public const int MAX_PLAYER_COUNT = 4;
+
+    public static readonly Color[] PLAYER_COLORS = { Color.Red, Color.Blue, Color.Yellow, Color.Green };
+}
