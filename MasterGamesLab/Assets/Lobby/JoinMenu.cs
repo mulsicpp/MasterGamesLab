a989c042694e0b2b216fa0891ef52ec91633b224
Gift2 <gift2lp4k@gmail.com>
2026-05-13 18:37:09 +0200

    added public lobby list to join menu

--- MasterGamesLab/Assets/Lobby/JoinMenu.cs
+++ MasterGamesLab/Assets/Lobby/JoinMenu.cs
@@ -1,48 +1,74 @@
 using UnityEngine;
 using UnityEngine.UIElements;
+using System.Collections.Generic;
 
 public class JoinMenu : MonoBehaviour
 {
+    private TextField _lobbyCodeInput;
+    private MultiColumnListView _lobbyList;
+    
 
-    [SerializeField] private Lobby lobby;
-    private Button joinButton;
-    private Button backButton;
+    private List<LobbyData> _availableLobbies = new List<LobbyData>();
 
-    private TextField nameField;
-    private TextField ipAdressField;
-
-
-    private VisualElement root;
-
-
-    void OnEnable()
+    private void OnEnable()
     {
-        root = GetComponent<UIDocument>().rootVisualElement;
+        var root = GetComponent<UIDocument>().rootVisualElement;
 
-        nameField = root.Q<TextField>("Name");
-        ipAdressField = root.Q<TextField>("IpAddress");
 
-        backButton = root.Q<Button>("Back");
+        var backButton = root.Q<Button>("BackButton");
+        var joinButton = root.Q<Button>("JoinButton");
+        _lobbyCodeInput = root.Q<TextField>("LobbyCode");
+
         backButton.clicked += OnBackPressed;
-
-        joinButton = root.Q<Button>("Join");
         joinButton.clicked += OnJoinPressed;
 
+
+        _lobbyList = root.Q<MultiColumnListView>("LobbyList");
+        SetupLobbyList();
+        AddLobbyElement("test1", 2);
+        AddLobbyElement("test2", 3);
+        AddLobbyElement("test3", 4);
     }
 
+    public void OnBackPressed()
+    {
+        Debug.Log("Back button clicked. Returning to Main Menu...");
 
-    private void OnJoinPressed()
-    {
-        Debug.Log("Name: " + nameField.text + ", IP Address: " + ipAdressField.text);
-    }
-    private void OnBackPressed()
-    {
-        lobby.Show();
-        gameObject.SetActive(false);
     }
 
-    public void Show()
+    public void OnJoinPressed()
     {
-        gameObject.SetActive(true);
+        string code = _lobbyCodeInput.value;
+        Debug.Log($"Attempting to join with code: {code}");
     }
+
+    // This is the function you requested to add new items dynamically
+    public void AddLobbyElement(string lobbyName, int playerCount)
+    {
+        _availableLobbies.Add(new LobbyData { Name = lobbyName, Players = playerCount });
+        
+        // Tell the UI Toolkit to refresh and show the new data
+        _lobbyList.RefreshItems();
+    }
+
+    private void SetupLobbyList()
+    {
+        _lobbyList.itemsSource = _availableLobbies;
+
+        // Binding the "Name" column
+        _lobbyList.columns["name"].makeCell = () => new Label();
+        _lobbyList.columns["name"].bindCell = (VisualElement e, int i) => 
+            (e as Label).text = _availableLobbies[i].Name;
+
+        // Binding the "Players" column
+        _lobbyList.columns["players"].makeCell = () => new Label();
+        _lobbyList.columns["players"].bindCell = (VisualElement e, int i) => 
+            (e as Label).text = $"{_availableLobbies[i].Players}/" + Constants.MAX_PLAYER_COUNT;
+    }
+}
+
+public class LobbyData
+{
+    public string Name;
+    public int Players;
 }
