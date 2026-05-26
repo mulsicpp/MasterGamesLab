b5ab897c3bdd7375893d3d2ecff132c335920929
myvvv <merrycaa@gmail.com>
2026-05-26 13:48:11 +0200

    map clearup skript, forest moutain generation reworked

--- MasterGamesLab/Assets/ProceduralGeneration/ForestMountainPass.cs
+++ MasterGamesLab/Assets/ProceduralGeneration/ForestMountainPass.cs
@@ -1,44 +1,66 @@
 using Map;
+using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
 public class ForestMountainPass : IGenerationPass
 {
     //smaller value = bigger patches, bigger value = smaller patches
-    public float noiseScale = 3.5f;
+    //public float noiseScale = 3.5f;
 
     //everything above 0.4 becomes mountain, everything below -0.25 becomes forest, everything in between is plain
-    public float mountainThreshold = 0.4f; 
-    public float forestThreshold = -0.25f; 
+    //forest
+    public float forestNoiseScale = 3.5f;
+    public float forestThreshold = -0.25f;
+
+    //mountain
+    public float mountainNoiseScale = 1.2f;
+    public float ridgeThickness = 0.11f; //0 is thin
+    public int minMountainChainLength = 10;
+
+    public float maskThreshold = -0.5f;
 
     public void Execute(IMap map)
     {
-        var noiseOffset = new float3(
-            UnityEngine.Random.Range(-100f, 100f),
-            UnityEngine.Random.Range(-100f, 100f),
-            UnityEngine.Random.Range(-100f, 100f)
-        );
+        var forestOffset = new float3(UnityEngine.Random.Range(-100f, 100f), UnityEngine.Random.Range(-100f, 100f), UnityEngine.Random.Range(-100f, 100f));
+        var mountainOffset = new float3(UnityEngine.Random.Range(-100f, 100f), UnityEngine.Random.Range(-100f, 100f), UnityEngine.Random.Range(-100f, 100f));
+        var mountainMaskOffset = new float3(UnityEngine.Random.Range(-100f, 100f), UnityEngine.Random.Range(-100f, 100f), UnityEngine.Random.Range(-100f, 100f));
 
-        var forestCount = 0;
-        var mountainCount = 0;
-
+        //var mountainCount = 0;
         foreach (var tile in map.Tiles)
         {
             if (tile.Type == Tile.TileType.Water) continue;
 
             var pos = new float3(tile.PositionOnSphere.x, tile.PositionOnSphere.y, tile.PositionOnSphere.z);
 
-            var n = noise.snoise((pos * noiseScale) + noiseOffset);
+            //var n = noise.snoise((pos * noiseScale) + noiseOffset);
+            //var forestNoise = noise.snoise((pos * forestNoiseScale) + forestOffset);
+            //if (forestNoise < forestThreshold)
+            //{
+            //    tile.Type = Tile.TileType.Forest;
+            //    continue; 
+            //}
 
-            if (n > mountainThreshold)
+            //if (n > mountainThreshold)
+            //{
+            //    tile.Type = Tile.TileType.Mountain;
+            //    mountainCount++;
+            //}
+            //else if (n < forestThreshold)
+            //{
+            //    tile.Type = Tile.TileType.Forest;
+            //    forestCount++;
+            //}
+            //else
+            //{
+            //    tile.Type = Tile.TileType.Plain;
+            //}
+            var ridgeNoise = math.abs(noise.snoise((pos * mountainNoiseScale) + mountainOffset));
+            var maskNoise = noise.snoise((pos * 1.5f) + mountainMaskOffset);
+
+            if (ridgeNoise < ridgeThickness && maskNoise > maskThreshold)
             {
                 tile.Type = Tile.TileType.Mountain;
-                mountainCount++;
-            }
-            else if (n < forestThreshold)
-            {
-                tile.Type = Tile.TileType.Forest;
-                forestCount++;
             }
             else
             {
@@ -46,6 +68,65 @@ public class ForestMountainPass : IGenerationPass
             }
         }
 
-        Debug.Log($"forest: {forestCount}, mountain: {mountainCount}");
+        var visitedMountains = new HashSet<ITile>();
+        var validMountainCount = 0;
+
+        foreach (var tile in map.Tiles)
+        {
+            if (tile.Type == Tile.TileType.Mountain && !visitedMountains.Contains(tile))
+            {
+                var currentChain = new List<ITile>();
+                var queue = new Queue<ITile>();
+
+                queue.Enqueue(tile);
+                visitedMountains.Add(tile);
+                currentChain.Add(tile);
+
+                //flood fill
+                while (queue.Count > 0)
+                {
+                    var current = queue.Dequeue();
+
+                    foreach (var neighbor in current.Neighbors)
+                    {
+                        if (neighbor.Type == Tile.TileType.Mountain && !visitedMountains.Contains(neighbor))
+                        {
+                            visitedMountains.Add(neighbor);
+                            currentChain.Add(neighbor);
+                            queue.Enqueue(neighbor);
+                        }
+                    }
+                }
+
+                if (currentChain.Count < minMountainChainLength)
+                {
+                    foreach (var mTile in currentChain)
+                    {
+                        mTile.Type = Tile.TileType.Plain;
+                    }
+                }
+                else
+                {
+                    validMountainCount += currentChain.Count;
+                }
+            }
+        }
+
+        var forestCount = 0;
+        foreach (var tile in map.Tiles)
+        {
+            if (tile.Type == Tile.TileType.Water || tile.Type == Tile.TileType.Mountain) continue;
+
+            var pos = new float3(tile.PositionOnSphere.x, tile.PositionOnSphere.y, tile.PositionOnSphere.z);
+            var forestNoise = noise.snoise((pos * forestNoiseScale) + forestOffset);
+
+            if (forestNoise < forestThreshold)
+            {
+                tile.Type = Tile.TileType.Forest;
+                forestCount++;
+            }
+        }
+        Debug.Log($"mountain Tiles: {validMountainCount} ");
+        Debug.Log($"forest Tiles: {forestCount} ");
     }
 }
